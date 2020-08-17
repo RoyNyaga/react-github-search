83f59a976f5df5f9f7a6c23393ed60796197413f
Roy Nyaga <nyagaandreroy@gmail.com>
2020-08-17 12:32:37 +0100

    complete followers component

--- src/components/Followers.js
+++ src/components/Followers.js
@@ -8,14 +8,17 @@ const Followers = () => {
   return <Wrapper>
             <div className="followers">
               {followers.map((follower,index)=>{
-                const {avarar_url:img,html_url,login} = follower;
+                const {avatar_url:img,html_url,login} = follower;
                 return (
                   <article key={index}>
                     <img src={img} alt={login}/>
+                    <div>
+                      <h4>{login}></h4>
+                      <a href={html_url}>{html_url}</a>
+                    </div>
+                    
                   </article>
                   )
-
-
               })}
             </div>
           </Wrapper>;
