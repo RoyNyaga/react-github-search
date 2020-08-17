22ce391027b5b70470a8a38033fb067e70b8b950
Roy Nyaga <nyagaandreroy@gmail.com>
2020-08-17 12:53:40 +0100

    render fusion chart

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -3,8 +3,10 @@ import styled from 'styled-components';
 import { GithubContext } from '../context/context';
 import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
 const Repos = () => {
+  const{repos} = React.useContext(GithubContext)
+  console.log(repos)
   return <h2>repos component</h2>;
-};
+}; 
 
 const Wrapper = styled.div`
   display: grid;
