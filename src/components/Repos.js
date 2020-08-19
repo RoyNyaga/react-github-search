47de53412a1ff9bdb23bcd7891fc0a3f15b2a57b
Roy Nyaga <nyagaandreroy@gmail.com>
2020-08-19 15:23:41 +0100

    add stars

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -38,6 +38,21 @@ const Repos = () => {
   }).slice(0,5)
 
   console.log(mostPopular)
+
+  // stars, forks
+
+  let {stars,forks} = repos.reduce((total,item) => {
+    const {stargazers_count,name,forks} = item
+    total.stars[stargazers_count] = {
+      label:name,value:stargazers_count
+    }
+    return total
+  },{
+    stars:{},forks:{}
+  })
+
+  stars = Object.values(stars).slice(-5).reverse();
+
   const chartData = [
     {
       label: "HTML",
@@ -55,7 +70,7 @@ const Repos = () => {
   return <section className="section">
             <Wrapper className="section-center">          
               <Pie3D data={mostUsedLanguage}/>
-              <Column3D data={chartData}/>
+              <Column3D data={stars}/>
               <Doughnut2D data={mostPopular}/>
               <Bar3D data={chartData} />
             </Wrapper>
