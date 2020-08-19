073400c17a5d0a063f15ffa57a775235ce649423
Roy Nyaga <nyagaandreroy@gmail.com>
2020-08-19 15:50:12 +0100

    add handleSubmit

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,7 +3,25 @@ import styled from 'styled-components';
 import { MdSearch } from 'react-icons/md';
 import { GithubContext } from '../context/context';
 const Search = () => {
-  return <h2>search component</h2>;
+  const [user,setUser] = React.useState("");
+  // get things from global context
+  const handleSubmit = () => {
+    e.preventDefault();
+    console.log(user)
+  }
+  return <section className="section">
+          <Wrapper className="section-center">
+            <form onSubmit={handleSubmit} >
+              <div className="form-control">
+                <MdSearch />
+                <input type="text" placeholder="enter github user" 
+                  onChange={(e)=>setUser(e.target.value)}/> 
+                <button type="submit">search</button>
+              </div>
+            </form>
+            <h3>request : 60 / 60</h3>
+          </Wrapper>
+        </section>;
 };
 
 const Wrapper = styled.div`
