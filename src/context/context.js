3f4bf4f0ca77c5fb0f858d1c7cc43b09865d109c
RoyNyaga <nyagaandreroy@gmail.com>
2020-08-31 23:26:32 +0100

    add checkRequests

--- src/context/context.js
+++ src/context/context.js
@@ -12,13 +12,25 @@ const GithubProvider = ({children}) => {
 	const [githubUser, setGithubUser] = useState(mockUser)
 	const [repos, setRepos] = useState(mockRepos)
 	const [followers, setFollowers] = useState(mockFollowers)
+	// check rate
+	const checkRequests = () => {
+		axios(`${rootUrl}/rate_limit`)
+		.then(({data}) => {
+			let { rate: { remaining }, } = data // notice this destructuring format
+			setRequests(remaining)
+			if(remaining === 0){
+				// throw an error
+			}
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+	}
 	// request loading
 	const [requests, setRequests] = useState(0);
-	const [loading, setIslaoding] = useState(false)
+	const [loading, setIslaoding] = useState(false);
 	// error
-	useEffect(()=>{
-		console.log("hey app loadded")
-	})
+	useEffect(checkRequests, [])
 	return(
 		<GithubContext.Provider value={{githubUser, repos, followers }}>{children}</GithubContext.Provider>
 		) 
