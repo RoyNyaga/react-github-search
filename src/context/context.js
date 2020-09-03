a4db6e2154e053f2ff8810e1abaa9e5ac6c5bbca
RoyNyaga <nyagaandreroy@gmail.com>
2020-09-03 23:55:28 +0100

    await Promise.allSettled

--- src/context/context.js
+++ src/context/context.js
@@ -33,7 +33,14 @@ const GithubProvider = ({children}) => {
 				[axios(`${rootUrl}/users/${login}/repos?per_page=100`), axios(`${followers_url}?per_page=100`)]
 			)
 			.then((results) => {
-				console.log(results)
+				const [repos, followers] = results;
+				const status = "fulfilled";
+				if(repos.status === status){
+					setRepos(repos.value.data)
+				}
+				if(followers.status === status){
+					setFollowers(followers.value.data)
+				}
 			})
 		}else{
 			toggleError(true, "there is no user with such Name")
