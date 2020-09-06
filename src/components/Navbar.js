e0b80670b4baab20d3b7db36c7786ddec4801923
RoyNyaga <nyagaandreroy@gmail.com>
2020-09-06 20:54:01 +0100

    add logout button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,9 +3,21 @@ import styled from 'styled-components';
 import { useAuth0 } from '@auth0/auth0-react';
 
 const Navbar = () => {
-  const {isAuthenticated, loginWithRedirect,logout,user,isLoading} = useAuth0();
+  const {
+    isAuthenticated,
+    loginWithRedirect,
+    logout,
+    user,
+    isLoading
+  } = useAuth0();
 
-  return <Wrapper><button onClick={loginWithRedirect}>login</button> </Wrapper>;
+  return (
+    <Wrapper>
+     <button onClick={loginWithRedirect}>login</button>
+     <button onClick={()=>{logout({returnTo:window.location.origin})}}>Logout</button>
+    </Wrapper>
+  )
+  
 };
 
 const Wrapper = styled.nav`
